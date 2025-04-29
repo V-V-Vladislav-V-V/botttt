fb5dba8f889d55e857a1ab10f41ce477fb646417
V-V-Vladislav-V-V <Vladislawgalitch@yandex.ru>
2025-04-29 20:58:12 +0700

    new files

--- botttt.py
+++ botttt.py
@@ -1,1 +1,17 @@
-print("hello world!")
+import asyncio
+import  logging
+from aiogram import Bot, dispatcher, types
+from aiogram.filters.command import Command
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="7575261191:AAFEgmlTbahKifPtOWsFL8rt1hksYBOhQ5s")
+disp = dispatcher()
+
+@disp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+async def main():
+    await disp.start_polling(bot)
+
+if __name__== "__main__":
+    asyncio.run(main())
